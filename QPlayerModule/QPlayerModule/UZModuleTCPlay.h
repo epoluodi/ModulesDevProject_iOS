ec1958f2756b99c9e7b749a747afd7aebefb516c
撒旦 <12163687@qq.com>
2016-05-30 22:41:45 +0800

    视频增加完成

--- QPlayerModule/QPlayerModule/UZModuleTCPlay.h
+++ QPlayerModule/QPlayerModule/UZModuleTCPlay.h
@@ -8,8 +8,24 @@
 
 #import <UIKit/UIKit.h>
 #import "UZModule.h"
-@interface UZModuleTCPlay : UZModule
+#import "NSDictionaryUtils.h"
+#import <TCPlayerSDK/TCPlayerSDK.h>
+#import "TCPlayItem.h"
+
+#define BtnControlWH 80;
+
+@interface UZModuleTCPlay : UZModule<TCPlayerEngineDelegate>
+{
+    TCPlayerView *_playerView;//播放器
+    UIView *_forceview;//背景图层
+    UIButton *_btncontrol;//控制按钮
+    BOOL isautoplay;//自动播放
+    NSString *videourl;
+
+}
 
 
 -(void)addbutton:(NSDictionary *)paramDict;
+//初始化播放插件
+-(void)initTCPlayView:(NSDictionary *)paramDict;
 @end
--- QPlayerModule/QPlayerModule/UZModuleTCPlay.m
+++ QPlayerModule/QPlayerModule/UZModuleTCPlay.m
@@ -17,25 +17,103 @@
 
 - (id)initWithUZWebView:(UZWebView *)webView_ {
     if (self = [super initWithUZWebView:webView_]) {
+        _playerView = [[TCPlayerView alloc] initControlView:nil bottomView:nil];
+        _forceview = [[UIView alloc] init];
+        [_forceview setBackgroundColor:[[UIColor blackColor] colorWithAlphaComponent:0.3]];
+        _btncontrol = [[UIButton alloc] init];
+        [_btncontrol setImage:[UIImage imageNamed:@"res_moduleDemo/play"] forState:UIControlStateNormal];
+        _playerView.playerDelegate = self;
         
+        [_btncontrol addTarget:self action:@selector(playControl) forControlEvents:UIControlEventTouchUpInside];
+        [_playerView showMask:nil];
     }
     return self;
 }
 
 - (void)dispose {
     //do clean
+    [_playerView stopAndRelease];
+    [_btncontrol removeFromSuperview];
+    [_forceview removeFromSuperview];
+    [_playerView removeFromSuperview];
+    _btncontrol =nil;
+    _forceview =nil;
+    _playerView =nil;
 }
 
--(void)addbutton:(NSDictionary *)paramDict
+
+
+
+
+
+-(void)initTCPlayView:(NSDictionary *)paramDict
 {
-    UIImageView *imgview = [[UIImageView alloc] init];
-//    imgview.image = [UIImage imageNamed:@"png/state0"];
-        [imgview setBackgroundColor:[UIColor redColor]];
+    NSLog(@"%@",paramDict);
+    NSInteger x,y,w,h;
+        x  = [paramDict integerValueForKey:@"x" defaultValue:0];
+    y  = [paramDict integerValueForKey:@"y" defaultValue:0];
+    w  = [paramDict integerValueForKey:@"w" defaultValue:0];
+    h  = [paramDict integerValueForKey:@"h" defaultValue:0];
+    videourl = [paramDict stringValueForKey:@"videoUrl" defaultValue:@""];
+    if (![paramDict valueForKeyIsNull:@"IsAutoPlay"])
+    {
+        isautoplay = [paramDict boolValueForKey:@"IsAutoPlay" defaultValue:NO];
+    }
+    _playerView.frame =CGRectMake(x, y, w, h);
+    _forceview.frame = CGRectMake(0, 0, _playerView.frame.size.width, _playerView.frame.size.height);
+    [_playerView addSubview:_forceview];
+    int _wh = BtnControlWH;
+    _btncontrol.frame = CGRectMake(w /2 - _wh /2, h/2  - _wh /2, _wh, _wh);
+    [_forceview addSubview:_btncontrol];
+       [self addSubview:_playerView fixedOn:nil fixed:NO];
     
-    imgview.frame = CGRectMake(0, 0, 200, 100);
-    [self addSubview:imgview fixedOn:nil fixed:YES];
+    
+    TCPlayItem *item = [[TCPlayItem alloc] init];
+    item.type = @"标清";
+    item.url = @"http://200000987.vod.myqcloud.com/200000987_2403d512254811e6ad0a9bb665111595.f210.av.m3u8";
+//http://200000987.vod.myqcloud.com/200000987_2403d512254811e6ad0a9bb665111595.f210.av.m3u8
+    
+    
+    if (isautoplay){
+        [_forceview setHidden:YES];
+        [_playerView play:item];
+    }
+    else
+    {
+      
+    }
+}
+
+
+#pragma mark 播放控制
+//播放控制
+-(void)playControl
+{
+    if([_playerView isPlaying])
+    {
+        [_playerView pause];
+        return;
+    }
+    if ([_playerView isPaused])
+    {
+        [_playerView play];
+        return;
+    }
     
 }
+
+
+#pragma mark -
+
+#pragma mark 播放回调事件
+
+//资源准备失败
+-(void)onPlayerFailed:(id<TCPlayerEngine>)player errorType:(TCPlayerErrorType)errType
+{
+    
+}
+
+#pragma mark  -
 
 
 @end
